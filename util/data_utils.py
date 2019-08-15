99bf0468376935ca01e46c9370bf30ca576b410f
varunnayak26 <varun.nayak@retest.de>
2019-08-15 11:57:34 +0200

    Add data_utils.py

---
+++ util/data_utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+from collections import Counter
+
+
+def create_sequence_for_sessions(df):
+    # group by session id and concat item_id
+    groups = df.groupby('SessionId')
+    # convert item ids to string, then aggregate them to lists
+    aggregated = groups['ItemId'].apply(lambda x: list(map(str, x)))
+    aggregated = aggregated.to_frame(name='sequence')
+
+    init_ts = groups['Time'].min()
+
+    result = aggregated.join(init_ts)
+    result.reset_index(inplace=True)
+    return result
+
+
+def get_dataset_info(df):
+    seq_df = create_seq_db_filter_top_k(df)
+    cnt = Counter()
+    seq_df.sequence.map(cnt.update)
+    sequence_length = seq_df.sequence.map(len).values
+    print('Number of items: {}'.format(len(cnt)))
+    print('Number of sessions: {}'.format(len(seq_df)))
+
+    print('\nSession length:\n\tAverage: {:.2f}\n\tMedian: {}\n\tMin: {}\n\tMax: {}'.format(
+        sequence_length.mean(),
+        np.quantile(sequence_length, 0.5),
+        sequence_length.min(),
+        sequence_length.max()))
+
+    print('Top 5 most common items as (item,count): {}'.format(cnt.most_common(5)))
