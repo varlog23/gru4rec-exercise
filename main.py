229d3ac68caa5ca794d61f5d71dad76cbe39f0d1
varunnayak26 <varun.nayak@retest.de>
2019-09-05 17:10:40 +0200

    Add main.py

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
 import preprocess
-import pandas as pd
-import numpy as np
-from gru4rec.gru4rec import GRU4Rec
+import util.data_exploration
 import util.data_utils
+import util.gendata
 from Recommender import Recommender
+from util.split import test_train_split
+
+train_file_path = r'/home/varun/Downloads/Dataset/TrainAction/'
 
 
-def main():
-    data = preprocess.get_data()
-    print('Shape of dataframe is: {}'.format(data.shape))
+def generate_synthetic_data(maxid=0):
+    synthetic_data = util.gendata.generate_sequences(1, maxid+1, 50, 150)
+
+    temp_df = util.gendata.generate_sequences(2, synthetic_data['SessionId'].max()+1, 160, 300)
+    synthetic_data = synthetic_data.append(temp_df)
+
+    temp_df = util.gendata.generate_sequences(1, synthetic_data['SessionId'].max()+1, 1, 500)
+    synthetic_data = synthetic_data.append(temp_df)
+
+    temp_df = util.gendata.generate_sequences(4, synthetic_data['SessionId'].max()+1, 400, 500)
+    synthetic_data = synthetic_data.append(temp_df)
+
+    temp_df = generate_noise(synthetic_data['SessionId'].max())
+    synthetic_data = synthetic_data.append(temp_df)
+
+    return synthetic_data
+
+
+def generate_noise(maxid=0):
+    noisy_data = util.gendata.generate_noise(maxid+1, 1, 500, minseq=1, maxseq=50, numseqs=150)
+
+    temp_df = util.gendata.generate_noise(noisy_data['SessionId'].max() + 1, 1, 500, minseq=1, maxseq=15, numseqs=50)
+    noisy_data = noisy_data.append(temp_df)
+
+    noisy_data_as_sessions = util.data_utils.create_sequence_for_sessions(noisy_data)
+    print(noisy_data_as_sessions)
+    # Plot noise
+    # util.data_exploration.plot_distribution(noisy_data)
+    # util.data_exploration.plot_count(noisy_data)
+    return noisy_data
+
+
+if __name__ == "__main__":
+    # Import data
+    data, dict_of_items = preprocess.get_data(train_file_path)
+
+    # Append Synthetic data(contains noise)
+    data = data.append(generate_synthetic_data(data['SessionId'].max()))
+
+    # Split dataset
+    data, valid = test_train_split(data, 1.0)
     util.data_utils.print_dataset_info(data)
+    data_as_sessions = util.data_utils.create_sequence_for_sessions(data)
+    print(data_as_sessions)
 
-    gru = GRU4Rec(loss='bpr-max', final_act='elu-0.5', hidden_act='tanh', layers=[100], adapt='adagrad',
-                  n_epochs=10, batch_size=2, dropout_p_embed=0, dropout_p_hidden=0, learning_rate=0.2,
-                  momentum=0.3, n_sample=2048, sample_alpha=0, bpreg=1, constrained_embedding=False)
-    gru.fit(data)
+    # Plot data to visualize
+    util.data_exploration.plot_distribution(data)
+    util.data_exploration.plot_count(data)
 
-
-def main_using_interface():
-    data = preprocess.get_data()
-    print('Shape of dataframe is: {}'.format(data.shape))
-    util.data_utils.print_dataset_info(data)
-
+    # Instantiate the model
     recommender = Recommender(session_layers=[100],
                               batch_size=2,
                               learning_rate=0.01,
@@ -31,6 +67,15 @@ def main_using_interface():
                               personalized=False)
     recommender.fit(data)
 
+    # Test manually for prediction
+    item_predicted = recommender.recommend(['1'])
+    print('Prediction for [1] is: {}'.format(item_predicted))
 
-if __name__ == "__main__":
-    main()
+    item_predicted = recommender.recommend(['8', '9'])
+    print('Prediction for [8,9] is: {}'.format(item_predicted))
+
+    item_predicted = recommender.recommend(['1', '2'])
+    print('Prediction for [1,2] is: {}'.format(item_predicted))
+
+    item_predicted = recommender.recommend(['1', '2', '3'])
+    print('Prediction for [1,2,3] is: {}'.format(item_predicted))
